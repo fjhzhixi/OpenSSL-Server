834baff93b8b3c43183a5e1e350721c5c3a43708
DoubleRider <xshidaiguanghuaq@163.com>
2020-05-10 10:55:34 +0800

    update_website

--- website/core.py
+++ website/core.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, session, jsonify
 from werkzeug.utils import secure_filename
-from function import *
+from sql import *
 
 import time
 import os
@@ -12,103 +12,85 @@ UPLOAD_FOLDER = "upload"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 basedir = os.path.abspath(os.path.dirname(__file__))
 ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'xls', 'JPG', 'PNG', 'xlsx', 'gif', 'GIF'])
+tishiforindex = None
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-sys = Sql()
-account_type = 0
-
-@app.route('/test/upload')
-def test_upload():
-    return render_template('upload.html')
+sys = Sql('xuqiang')    #此处修改数据库登录密码
 
 @app.route('/api/upload', methods = ['POST'], strict_slashes = False)
 def api_upload():
-    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
+    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'], session.get('userid'))
     if not os.path.exists(file_dir):
         os.makedirs(file_dir)
     f = request.files['myfile']
     if f and allowed_file(f.filename):
         fname = secure_filename(f.filename)
         print(fname)
-        ext = fname.rsplit('.', 1)[1]
-        unix_time = int(time.time())
-        new_filename = str(unix_time) + '.' + ext
-        f.save(os.path.join(file_dir, new_filename))
-        token = new_filename
-        print('token='+token)
-        return redirect(url_for('index'))
+        fpath = os.path.join(file_dir, fname)
+        try:
+            print(fpath)
+            sys.upload_file(fname, fpath)
+        except HasnotSigninException as err:
+            tishiforindex = "用户未登录！"
+            return redirect(url_for('index'))
+        else:
+            f.save(os.path.join(file_dir, fname))
+            tishiforindex = "上传成功！"
+            return redirect(url_for('index'))
     else:
-        return jsonify({"errno":1001, "errmsg": "上传失败"})
+        tishiforindex = "文件格式不符合要求"
+        return redirect(url_for('index'))
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
     if request.method == 'POST':
         print("registering")
-        user_id = sys.sign_up(int(request.form['type']), request.form['name'], request.form['password'])
-        return render_template('login.html', error = None, tishi = 'user_' + user_id)
-    return render_template('register.html')
+        try:
+            sys.sign_up(request.form['userid'], request.form['name'], request.form['password'])
+        except AccountAlreadyExistError as err:
+            return render_template('register.html', tishi = err)
+        else:
+            return redirect(url_for('login'))
+    return render_template('register.html', tishi = None)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
         try:
-            sys.sign_in(request.form['accout'][5:], request.form['password'])
+            sys.sign_in(request.form['userid'], request.form['password'])
         except NoneAccountFoundError as err:
             return render_template('login.html', error = err, tishi = None)
         except MultAccountFoundError as err:
             return render_template('login.html', error = err, tishi = None)
         else:  
-            session['user_id'] = request.form['accout'][5:]
-            session['user_type'] = str(sys.get_cur_account_type())
+            session['userid'] = request.form['userid']
             return redirect(url_for('index'))
-
     return render_template('login.html', error = None, tishi = None)
 
 @app.route('/logout')
 def logout():
     sys.sign_out()
-    session.pop('user_id')
+    session.pop('userid')
     return redirect(url_for('login'))
 
 @app.route('/')
 def index():
-    user_id = session.get('user_id')
-    user_type = session.get('user_type')
-    normal = 1
-    if (user_type == "0"):
-        normal = None
-    provider = None
+    user_id = session.get('userid')
     if user_id == None:
         print('failure')
         return redirect(url_for('login'))
-    print('continue')
-    user_name = sys.get_cur_account_name()
-    game = sys.select_game_id()
-    games = []
-    for each in game:
-        ans = sys.select_game(each)
-        p = sys.select_provider(str(ans['provider']))
-        name = p['provider_name']
-        ans['provider_name'] = name
-        allgame = sys.select_owned_game_id()
-        add = 0
-        num = 0
-        for one in allgame:
-            if (str(one[1]) == str(ans['game_id'])):
-                sea = sys.select_owned_game(str(one[0]), str(one[1]))
-                if sea['game_evaluation'] != None:
-                    add = add + sea['game_evaluation']
-                    num = num + 1
-        if num == 0:
-            ans['evaluation'] = 100
-        else:
-            ans['evaluation'] = add/num
-        games.append(ans)
-    if (user_type == '2'):
-        provider = 1
-    return render_template('index.html', game = games, name = user_name, id = user_id, provider = provider, normal = normal)
+    user_name = sys.get_curent_user_name()
+    fileids = sys.select_all_fileid()
+    filelist = []
+    for each in fileids:
+        file = dict()
+        filename = sys.select_file_name(each)
+        file['name'] = filename.rsplit('.', 1)[0]
+        file['postfix'] = filename.rsplit('.', 1)[1]
+        filelist.append(file)
+    return render_template('index.html', file = filelist, name = user_name, tishi = None)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
